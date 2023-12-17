238e23eb3e157b20025dd678c200795f065ff36a
Mathias Olsson <matholss@kth.se>
2023-12-17 15:09:03 +0100

    added some views and also made a list for weight and reps for each set

---
+++ WorkoutDiary/View/WorkoutView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutView.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-17.
+//
+
+import SwiftUI
+
+struct WorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutView()
+}
