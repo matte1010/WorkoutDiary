b1a18d838d4f4a53775a21ae7b6754ab4f9d54dc
Mathias Olsson <matholss@kth.se>
2023-12-17 12:08:05 +0100

    Initial Commit

---
+++ WorkoutDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutDiary/WorkoutDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutDiaryApp.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-17.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
