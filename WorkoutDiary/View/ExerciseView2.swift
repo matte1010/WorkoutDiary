d88c76900c48b0530bb8f54b28212a7250d18496
Mathias Olsson <matholss@kth.se>
2023-12-17 21:45:16 +0100

    Made the exercise view work. Can remove keyboard, add reps and weight. Can remove items

---
+++ WorkoutDiary/View/ExerciseView2.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseView2.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-17.
+//
+
+import SwiftUI
+
+struct ExerciseView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseView2()
+}
