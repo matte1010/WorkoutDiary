0d7eea8b7677c5475e9eaa8f17f6f0c219c8b71f
Mathias Olsson <matholss@kth.se>
2023-12-18 18:25:32 +0100

    made ninjaapi work in testView

--- WorkoutDiary/Model/NinjaAPI.swift
+++ WorkoutDiary/Model/NinjaAPI.swift
@@ -13,24 +13,41 @@ class NinjaAPI: ObservableObject
 
     @Published var exercises = [Exercises]()
 
-       func loadData(completion:@escaping ([Exercises]) -> ()) 
-    {
-        let muscle = "biceps".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-        let url = URL(string: "https://api.api-ninjas.com/v1/exercises?muscle="+muscle!)!
+    func loadData(completion: @escaping ([Exercises]) -> ()) {
+        let query = "biceps".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let url = URL(string: "https://api.api-ninjas.com/v1/exercises?muscle=" + query!)!
         var request = URLRequest(url: url)
         request.setValue("zMoGk9CWn1sKaLAHYDKrzA==Yt5VgZpBgYNbebEx", forHTTPHeaderField: "X-Api-Key")
-        let task = URLSession.shared.dataTask(with: request) 
-        { (data, response, error) in
-            guard let data = data else { return }
-            print(String(data: data, encoding: .utf8)!)
-        }
-        task.resume()
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+                // Handle error appropriately, e.g., return an empty array or show an error message
+                DispatchQueue.main.async {
+                    completion([])
+                }
+                return
+            }
+
+            do {
+                let exercises = try JSONDecoder().decode([Exercises].self, from: data!)
+                print(exercises)
+                DispatchQueue.main.async {
+                    completion(exercises)
+                }
+            } catch {
+                print("Error decoding JSON: \(error)")
+                // Handle decoding error appropriately, e.g., return an empty array or show an error message
+                DispatchQueue.main.async {
+                    completion([])
+                }
+            }
+        }.resume()
     }
+
 }
 
-struct Exercises: Decodable
-{
-    //var id = UUID()
+struct Exercises: Decodable {
     var name: String
     var type: String
     var muscle: String
---
+++ WorkoutDiary/View/TestView.swift
@@ -0,0 +1,30 @@
+//
+//  TestView.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-18.
+//
+
+import SwiftUI
+
+struct TestView: View {
+
+    @State var exercises = [Exercises]()
+    @State var query: String = ""
+    
+    func getExercises() {
+        NinjaAPI().loadData { (exercises) in
+            self.exercises = exercises
+        }
+    }
+    
+    var body: some View {
+        Button("Button") {
+            getExercises()
+        }
+    }
+}
+
+#Preview {
+    TestView()
+}
