e2e0e26c015743e4f982daad845066b3682d17dc
Mathias Olsson <matholss@kth.se>
2023-12-27 20:36:41 +0100

    added functionality to store workouts and create them. Also added so its possible to show and edit only the desired workout.

---
+++ WorkoutDiary/View/SavedWorkoutView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedWorkoutView.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-27.
+//
+
+import SwiftUI
+
+struct SavedWorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedWorkoutView()
+}
---
+++ WorkoutDiary/View/WorkoutPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkoutPickerView.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-27.
+//
+
+import SwiftUI
+
+struct WorkoutPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkoutPickerView()
+}
