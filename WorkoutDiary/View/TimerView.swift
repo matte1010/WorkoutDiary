c011b75ee0fc54c5124fa0f9eaea16e61cb80bd4
Mathias Olsson <matholss@kth.se>
2023-12-28 15:34:30 +0100

    added a timerView and also added some comments. Implemented the timerView in the SavedWorkoutView but still needs improvements

---
+++ WorkoutDiary/View/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-28.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
