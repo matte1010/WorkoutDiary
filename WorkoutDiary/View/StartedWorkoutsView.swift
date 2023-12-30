121cb761150bb8180ca69e9d6dd2c29925d5a092
Mathias Olsson <matholss@kth.se>
2023-12-30 12:10:34 +0100

    Added three Views StartWorkoutView which should start a predefined workout, StartedWorkoutView which displays a startedWorkout and StartedWorkoutsView which displays all the workouts that has been started and shows the amount of sets

---
+++ WorkoutDiary/View/StartedWorkoutsView.swift
@@ -0,0 +1,18 @@
+//
+//  StartedWorkoutsView.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2023-12-29.
+//
+
+import SwiftUI
+
+struct StartedWorkoutsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartedWorkoutsView()
+}
