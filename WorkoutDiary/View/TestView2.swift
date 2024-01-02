74bf4cf5c374e809842983aa5a18c75eaa0127e0
Mathias Olsson <matholss@kth.se>
2024-01-02 17:00:27 +0100

    added Arins changes

---
+++ WorkoutDiary/View/TestView2.swift
@@ -0,0 +1,18 @@
+//
+//  TestView2.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2024-01-02.
+//
+
+import SwiftUI
+
+struct TestView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView2()
+}
