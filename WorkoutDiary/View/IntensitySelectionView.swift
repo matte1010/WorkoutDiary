924b2802333a93fa02e9f7e32f06fb0f0ea0e94a
Mathias Olsson <matholss@kth.se>
2024-01-06 23:15:00 +0100

    Added IntensitySelectionView and ReorderExerciseView (does not work)

---
+++ WorkoutDiary/View/IntensitySelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  IntensitySelectionView.swift
+//  WorkoutDiary
+//
+//  Created by Mathias Olsson on 2024-01-06.
+//
+
+import SwiftUI
+
+struct IntensitySelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntensitySelectionView()
+}
